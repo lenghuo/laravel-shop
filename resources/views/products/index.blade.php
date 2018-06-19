cf35dd4ad992ef71ece713db26659cb35b11d605
lenghuo <weixupeng999@163.com>
2018-06-19 08:43:49 +0000

    用户界面 - 商品详情页

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -25,19 +25,23 @@
     <!-- 筛选组件结束 -->
     <div class="row products-list">
       @foreach($products as $product)
-      <div class="col-xs-3 product-item">
-        <div class="product-content">
-          <div class="top">
-            <div class="img"><img src="{{ $product->image_url }}" alt=""></div>
-            <div class="price"><b>￥</b>{{ $product->price }}</div>
-            <div class="title">{{ $product->title }}</div>
-          </div>
-          <div class="bottom">
-            <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
-            <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+        <div class="col-xs-3 product-item">
+          <div class="product-content">
+            <div class="top">
+              <div class="img">
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                  <img src="{{ $product->image_url }}" alt="">
+                </a>
+              </div>
+              <div class="price"><b>￥</b>{{ $product->price }}</div>
+              <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+            </div>
+            <div class="bottom">
+              <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
+              <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+            </div>
           </div>
         </div>
-      </div>
       @endforeach
     </div>
     <div class="pull-right">{{ $products->appends($filters)->render() }}</div>  <!-- 只需要添加这一行 -->
