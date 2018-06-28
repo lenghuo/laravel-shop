8e9954487d248a79369e52bb3fa82e25f903a534
lenghuo <weixupeng999@163.com>
2018-06-28 06:58:37 +0000

    订单

---
+++ database/migrations/2018_06_21_011915_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('no')->unique();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->referencs('id')->on('users')->onDelete('cascade');
+            $table->text('address');
+            $table->decimal('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
